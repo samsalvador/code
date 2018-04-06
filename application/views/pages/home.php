0fb5c47ef6e5edefcffb619ae2f7de79f8eed4c9
ite152a-32@M307.UC-BCF.EDU.PH <ite152a-32@M307.UC-BCF.EDU.PH>
2018-04-06 16:09:42 +0800

    sam

---
+++ application/views/pages/home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css"> 
+
+</head>
+<body>
+
+<div class="jumbotron" style="width:1928px;height:848px;background-image:url('https://i1.trekearth.com/photos/59762/img_0788.jpg')">
+	<br><br><br><br><br><br><br><br><br><br><br>
+  <CENTER><h1 class="display-3" style="color:white;">THE BEST RAMEN EVER</h1>
+  <h3 style="color:white;">We made our noodles in house in the style of traditional chukamen, using only flour and alkaline water.</h3>
+
+
+	</CENTER>
+	</div>
+
+
+		<div class="row">
+			<div class="gallery" >
+			  <a target="_blank" href="fjords.jpg">
+			    <img src="https://static1.squarespace.com/static/531c07b6e4b04c1bc678e0e6/575a51ee45bf214ad4d1578e/575a540060b5e91314ef0d48/1465537538052/spicychicken.jpg?format=500w" alt="Fjords" width="500" height="300">
+			  </a>
+
+			</div>
+
+			<div class="gallery">
+			  <a target="_blank" href="forest.jpg">
+			    <img src="https://static1.squarespace.com/static/531c07b6e4b04c1bc678e0e6/575a51ee45bf214ad4d1578e/575a540045bf214ad4d15dd0/1465537539431/shiotonkotsu.jpg?format=500w" alt="Forest" width="300" height="200">
+			  </a>
+
+			</div>
+
+			<div class="gallery">
+			  <a target="_blank" href="lights.jpg">
+			    <img src="https://static1.squarespace.com/static/531c07b6e4b04c1bc678e0e6/575a51ee45bf214ad4d1578e/575a53fe60b5e91314ef0d38/1465537536979/waffle2.jpg?format=500w" alt="Northern Lights" width="300" height="200">
+			  </a>
+
+			</div>
+
+			<div class="gallery">
+			  <a target="_blank" href="forest.jpg">
+			   <img src="https://static1.squarespace.com/static/531c07b6e4b04c1bc678e0e6/575a51ee45bf214ad4d1578e/575a540045bf214ad4d15dd0/1465537539431/shiotonkotsu.jpg?format=500w" alt="Forest" width="300" height="200">
+			  </a>
+			</div>
+
+
+	</div>
+	<center>
+		  <p class="lead">
+		    <a class="btn btn-primary btn-lg" href="#" role="button">Order Now</a>
+		  </p>
+		</center>
+		<br>
+		<br>
+</body>
+</html>
+
---
+++ application/views/templates/header.php
@@ -0,0 +1,50 @@
+<html>
+        <head>
+                <title>Sam</title>
+                	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/simplex/bootstrap.min.css"> 
+                	<link rel="stylesheet" type="text/css" href="main.css">
+                	<style>
+                 	.display-3{
+                		font-size: 100px;
+                	}
+                	div.gallery {
+					    margin: 150px;
+					    border: 1px solid #ccc;
+					    float: left;
+					    width: 380px;
+					    margin-right: auto;
+					    margin-left:auto;
+
+					}
+
+					div.gallery:hover {
+					    border: 1px solid #777;
+					}
+
+					div.gallery img {
+					    width: 100%;
+					    height: auto;
+
+					}
+
+					div.desc {
+					    padding: 15px;
+					    text-align: center;
+					}
+
+                	</style>
+        </head>
+        <body>
+        	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+			  <a class="navbar-brand" href="#">
+			  	<img src="http://static1.squarespace.com/static/531c07b6e4b04c1bc678e0e6/t/5771d811e58c622f5581d8dd/1517514907424/?format=1500w" alt="ramen logo" style="width:200px;height:70px;"></a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+
+
+			  </div>
+			</nav>
+
